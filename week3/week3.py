ef46a1675d42face9f4fd9591b030dc4def64f1a
Hsin-I Chang <nancy65018@gmail.com>
2022-01-13 11:36:33 +0800

    commit

--- week3/week3.py
+++ week3/week3.py
@@ -1,6 +1,5 @@
 from urllib.request import urlopen
-import pandas as pd
-import json
+import json,csv
 
 # 載入資料
 response = urlopen('https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json')
@@ -8,16 +7,20 @@ jsonData = response.read()
 data = json.loads(jsonData)
 
 # 整理資料
-df = pd.json_normalize(data['result']['results'])
-df['address'] = df['address'].str.slice(start=5,stop=8)
-df['file'] = df['file'].str.split(r'.jpg|.JPG')
+filelist = [];output = []
+mainData = data['result']['results']
 
-pic = []
-for i in range(len(df)):
-    pic.append(df['file'][i][0] + '.jpg')
-df['new_file'] = pic
+for i in range(len(mainData)):
+    out = []
+    out.append(mainData[i]['stitle'])
+    out.append(mainData[i]['address'][5:8])
+    out.append(mainData[i]['longitude'])
+    out.append(mainData[i]['latitude'])
+    filelist = mainData[i]['file'].split('https')
+    out.append('https' + filelist[1])
+    output.append(out)
 
-# 整理成獨立的 DataFrame 並匯出
-new_data = {'stitle':df['stitle'],'area':df['address'],'long':df['longitude'],'lat':df['latitude'],'file':df['new_file']}
-df2 = pd.DataFrame(new_data)
-df2.to_csv('data.csv',index=False,header=None)
+# 輸出資料
+with open('data.csv','w',encoding='utf-8',newline='') as file:
+    write = csv.writer(file)
+    write.writerows(output)
